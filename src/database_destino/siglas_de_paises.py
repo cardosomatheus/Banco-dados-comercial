423e4fe578fa7daab3aea85d1147dbd4b28c36fd
matheus <cardosomcds1@gmail.com>
2024-12-24 14:43:17 -0300

    Mudança de obtivo do projeto.

--- src/database_destino/siglas_de_paises.py
+++
@@ -1,8 +0,0 @@
-﻿import pdfplumber
-
-with pdfplumber.open('tabela-sigla-pais.pdf') as pdf:
-    for page in pdf.pages:        
-        # Extrair siglas dos paises
-        text = page.extract_text().split("\n")
-        for line in text:
-            print(f"Descrição: {line[:-2]}, Sigla: {line[-2:]}")
