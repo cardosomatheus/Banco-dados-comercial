46e362153ebd48e765e87688daab47e22acfff88
matheus <cardosomcds1@gmail.com>
2024-12-24 15:17:59 -0300

    arquivo config.py

---
+++ src/config.py
@@ -0,0 +1,16 @@
+﻿import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+# dados de conexao do banco
+DB_USUARIO = os.getenv("POSTGRES_USER_ORIGEM")
+DB_SENHA   = os.getenv("POSTGRES_PASSWORD_ORIGEM")
+DATABASE   = "origem",
+DB_HOST    = "localhost",
+DB_PORT    = "5432"
+
+
+
+
+DATA_DIR = "./data"
--- src/database_origem/conexao_origem.py
+++ src/database_origem/conexao_origem.py
@@ -1,23 +1,22 @@
-﻿import os
-from dotenv import load_dotenv
-import psycopg2
+﻿import psycopg2
+from src.config import DATABASE, DB_HOST, DB_PORT, DB_SENHA, DB_USUARIO
 
 
 
-class ConexaoBancoOrigem:
-    load_dotenv()
-    
-    def __init__(self):
-        self.usuario = os.getenv("POSTGRES_USER_ORIGEM")
-        self.senha   = os.getenv("POSTGRES_PASSWORD_ORIGEM")
-
-    def conectar_banco_origem(self):
-        return psycopg2.connect(database="origem",
-                                host="localhost",
-                                user=self.usuario,
-                                password= self.senha,
-                                port="5432")
+def conectar_banco_origem():
+    try:
+        print(f"Conectando ao banco {DB_HOST} com o usuário {DB_USUARIO}...")
+        #return psycopg2.connect(database= DATABASE,
+        #                    host    = DB_HOST,
+        #                    user    = DB_USUARIO,
+        #                    password= DB_SENHA,
+        #                    port    = DB_PORT)
+    except Exception as e:
+        print('Erro ao se conectar com o banoc: ', e)
+        return None
 
 
-
+#with conectar_banco_origem().cursor() as cursor:
+#    cursor.execute('Select count(*) from TB_PAIS')
+#    print(cursor.fetchone())
 
