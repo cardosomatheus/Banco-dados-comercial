c1227a925da266aa9b0fa576678d685c14544c51
matheus <cardosomcds1@gmail.com>
2024-12-26 11:14:57 -0300

    Refatoramento no script de Paises.

--- src/scripts/models/conexao_bd.py
+++ src/scripts/models/conexao_bd.py
@@ -1,17 +1,24 @@
 ﻿import psycopg2
 import os
 import sys
+from contextlib import contextmanager
 sys.path.insert(0,os.path.abspath('.'))
 
 from src.config.config import DATABASE, DB_HOST, DB_PORT, DB_SENHA, DB_USUARIO
 
-
+@contextmanager
 def conexao_bd():
     try:
-        return psycopg2.connect(database= DATABASE,
+        conn = psycopg2.connect(database= DATABASE,
                                 host    = DB_HOST,
                                 user    = DB_USUARIO,
                                 password= DB_SENHA,
                                 port    = DB_PORT)
+        yield conn
+        
     except Exception as e:
         print(e)
+    
+    finally:
+        if conn:
+            conn.close()
